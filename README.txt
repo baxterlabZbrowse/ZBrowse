3633cf674c093dd327b41a33f49c905bc483801a
rhartsock <rhartsock423@gmail.com>
2014-11-18 11:29:43 -0600

    Add install script.

--- README.txt
+++
@@ -1,8 +0,0 @@
-To install and run Z-Browse in R Studio:
-
-	At the R prompt in R Studio enter the command
-	
-		source("startup.R")
-		
-	This will check for the required packages and install them if they are not found.
-	You can use the same command to run Z-Browse in the future.
---
+++ startup.R
@@ -0,0 +1,11 @@
+packages <- c("shiny", "plyr", "tools", "xtable", "devtools", "markdown")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}
+
+if(!is.element('rCharts', installed.packages()[,1])){
+  require(devtools)
+  install_github('rCharts', 'ramnathv')
+}
+
+shiny::runApp()
